f91eefc814fcaabf45cd331c5fe68ef98944be9b
Jonas Elmwall <elmwall@gmail.com>
2026-05-11 21:28:22 +0200

    added multiproject functionality, major debugging, started on installation feature

--- install/install.py
+++ install/install.py
@@ -1,2 +1,67 @@
+import os
+import json
+
 import streamlit as st
+
+from src.a_define_project import run_define_project
+from src.b_define_labels import run_define_labels
+from src.c_define_event_terms import run_define_event_terms
+from src.d_define_limits import run_define_event_limits
+import utils.init as init
+import utils.tools as tools
+from config import SET_WIDTH, SET_HEIGHT
+# import form
+
+
+# col_main, col_dev = st.columns([8, 2])
+
+
+def welcome(set_width, set_height):
+    with st.container(border=False, key="guide", width=set_width, height=set_height, horizontal_alignment="center"):
+        col_prev, col_space, col_title, col_apply, col_next = st.columns([2, 2, 5, 2, 2])
+        col_title.markdown("#### Welcome to *PitySake*", text_alignment="center")
+
+        tools.navigate(col_prev, col_next, page=1)
+        # col_title.title("Welcome to *PitySake*")
+
+        st.markdown("""
+        here's somme info
+        """)
+        st.write("test")
+
+        # return 
+
+# st.sidebar.title("dsfsdf")
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+col_page_left, col_page_center, col_page_right = st.columns([1, 12, 1])
+
+# with open("config_template.json", "r", encoding="utf-8") as f:
+#     settings = json.load(f)
+
+# pages = [
+#     st.Page("pages/1_welcome.py", title="Welcome to PitySake")
+# ]
+# pg = st.navigation(pages)
+
+# if "initialized" not in st.session_state:
+init.initialize()
+if st.session_state["initialized"]:
+    with col_page_center.container(horizontal_alignment="center"):
+        if st.session_state["page"] == 0:
+            # pass
+            welcome(SET_WIDTH, SET_HEIGHT)
+        elif st.session_state["page"] == 1:
+            run_define_project(SET_WIDTH, SET_HEIGHT)
+        elif st.session_state["page"] == 2:
+            run_define_labels(SET_WIDTH, SET_HEIGHT)
+        elif st.session_state["page"] == 3:
+            run_define_event_terms(SET_WIDTH, SET_HEIGHT)
+        elif st.session_state["page"] == 4:
+            run_define_event_limits(SET_WIDTH, SET_HEIGHT)
+    # with col_page_left:
+        # pass
+    tools.dev_tools()
+#     pg.run()
+    # if info_page.welcome():
+    # form.run_form(settings)
 
---
+++ install/utils/init.py
@@ -0,0 +1,57 @@
+import json
+import streamlit as st
+
+
+
+INIT = {
+    "initialized": False,
+    # Page
+    "page": 0,
+    # Form
+    "submitted": dict(),
+    # Names
+    "ui_title": None,
+    "main": None,
+    "secondary": None,
+    "utility": None,
+    "attribute": None,
+    "origin": None,
+    "project_need_save": "secondary",
+    "project_is_changed": False,
+    # Labels
+    "label_1_number": 1,
+    "label_2_number": 1,
+    "label_3_number": 1,
+    "label_utility_1": None,
+    "label_attribute_1": None,
+    "label_origin_1": None,
+    "label_utility": None,
+    "label_attribute": None,
+    "label_origin": None,
+    "label_need_save": "secondary",
+    "label_is_changed": False,
+}
+
+def initialize():
+    for key, value in INIT.items():
+        if key not in st.session_state: st.session_state[key] = value
+
+    st.html("""
+    <style>
+        .st-key-guide p, li {font-weight: 300;}  
+        .st-key-name_save p {font-weight: 700;}
+        header {visibility: hidden;}  
+    </style>
+    """)
+
+    st.session_state["initialized"] = True
+    get_config_template()
+
+
+@st.cache_data
+def get_config_template():
+    with open("config_template.json", "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+
